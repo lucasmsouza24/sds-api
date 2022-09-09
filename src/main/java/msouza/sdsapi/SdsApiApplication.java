82271f2fa41331feb49a8e46d10079593048ba5c
lucasmsouza24 <lucasmesquita58@hotmail.com>
2022-09-09 16:43:12 -0300

    create structure

---
+++ src/main/java/msouza/sdsapi/SdsApiApplication.java
@@ -0,0 +1,13 @@
+package msouza.sdsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/msouza/sdsapi/SdsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package msouza.sdsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
