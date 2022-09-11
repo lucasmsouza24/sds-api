0ce5b4e4483e183f6b29276337c77e37283569cb
lucasmsouza24 <lucasmesquita58@hotmail.com>
2022-09-11 03:10:44 -0300

    structuring twilio message

--- src/main/java/msouza/sdsapi/controllers/SaleController.java
+++ src/main/java/msouza/sdsapi/controllers/SaleController.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -36,9 +37,9 @@ public class SaleController {
         return service.findSales(minDate, maxDate, pageable);
     }
 
-    @PostMapping("/notification")
-    public ResponseEntity<Message> notifySms() {
-        return ResponseEntity.created(URI.create("/api/sales/notification")).body(smsService.sendSms());
+    @PostMapping("/{id}/notification")
+    public ResponseEntity<Message> notifySms(@PathVariable Long id) {
+        return ResponseEntity.created(URI.create("/api/sales/notification")).body(smsService.sendSms(id));
     }
 
 }
--- src/main/java/msouza/sdsapi/services/SmsService.java
+++ src/main/java/msouza/sdsapi/services/SmsService.java
@@ -1,10 +1,13 @@
 package msouza.sdsapi.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
+import msouza.sdsapi.entities.Sale;
+import msouza.sdsapi.repositories.SaleRepository;
 
 @Service
 public class SmsService {
@@ -22,12 +25,30 @@ public class SmsService {
     @Value("${twilio.phone.to}")
     private String twilioPhoneTo;
 
+    @Autowired
+    private SaleRepository saleRepository;
+
     // methods
-    public Message sendSms() {
+    public Message sendSms(Long saleId) {
+        String msg = getMessage(saleId);
+
+        // twilio config
         Twilio.init(twilioSid, twilioKey);
         PhoneNumber to = new PhoneNumber(twilioPhoneTo);
         PhoneNumber from = new PhoneNumber(twilioPhoneFrom);
-        return Message.creator(to, from, "Teste").create();
+
+        // sending message
+        return Message.creator(to, from, msg).create();
+    }
+
+    private String getMessage(Long saleId) {
+        Sale sale = saleRepository.findById(saleId).get();
+        String date = sale.getDate().getDayOfMonth() + "/" + sale.getDate().getYear();
+        return String.format(
+            "O vendedor %s foi destaque em %s com um total de R$ %.2f",
+            sale.getSellerName(), 
+            date,
+            sale.getAmount());
     }
 
 }
